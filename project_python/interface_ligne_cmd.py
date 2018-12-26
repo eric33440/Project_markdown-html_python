1dea6c9cf938af772b90fad282452e3a42d9fdfd
rebillon <rebillon33440@gmail.com>
2018-12-26 17:09:27 +0100

    ajout des argument

--- project_python/interface_ligne_cmd.py
+++ project_python/interface_ligne_cmd.py
@@ -9,24 +9,45 @@ import getopt
 
 
 def main():
+    # extern = input()
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-v", "--verbose", help="increase output verbosity", action="store_true"
     )
-    parser.add_argument("-i", "--input-directory")
-    parser.add_argument("-o", "--output-directory")
+    parser.add_argument(
+        "-i",
+        "--input-directory",
+        help="define a folder for the file in markdown",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-directory",
+        help="define a folder for the file of static site",
+    )
+    parser.add_argument(
+        "-t",
+        "--template-directory",,
+    )
+    parser.add_argument(
+        "-h",
+        "--help",
+        help="define a folder for the file of static site",
+    )
     args = parser.parse_args()
+    if args == "-i":
+        file = glob.glob(
+            "/home/eric/workspace_python/project_python/fichier source/*.md"
+        )
+        print(
+            "les fichiers mardown se trouve : /home/eric/workspace_python/project_python/fichier source/"
+        )
+        print("Et il y a :{file}")
     if args.verbose:
         print("verbosity turned on")
-    file = glob.glob("/home/eric/workspace_python/project_python/*.md")
-    parser = argparse.ArgumentParser()
-    parser.add_argument("bar", type=argparse.FileType("w"))
-    parser.parse_args(["out.md"])
-    Namespace(bar=<_io.TextIOWrapper name='out.md' encoding='UTF-8'>)
 
 
     # glob.glob permet de trouver un motif et retourne une liste d'element contenant ce motif
-    # pour notre cas cela retourne tout les fichiers contenant
+    # pour notre cas cela retourne tout les fichiers markdown dans le chemin indique
     # with open(file,"r")file:
     #    file.read(line)
     # with open("", "w") as file:
